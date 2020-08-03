deaf51cd9cc61bcc8c2288e097f3b55075c3ca32
Stephen Junge <steve.junge@gmail.com>
2020-08-03 20:02:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  steves_array = ["wow", "tv", "desk", "couch", "table", "arrays!"]
+  steves_array = ["wow", "am", "desk", "couch", "table", "arrays!"]
   steves_array[1]
   
 end
