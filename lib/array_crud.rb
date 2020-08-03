2bc2e63912bb50a24078db98320797792ae98aad
Stephen Junge <steve.junge@gmail.com>
2020-08-03 20:01:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,33 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  steves_array = ["tv", "desk", "couch", "table"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  steves_array = ["tv", "desk", "couch", "table"]
+  steves_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  steves_array = ["tv", "desk", "couch", "table"]
+  steves_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  steves_array = ["tv", "desk", "couch", "table", "arrays!"]
+  steves_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  steves_array = ["wow", "tv", "desk", "couch", "table", "arrays!"]
+  steves_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  steves_array = ["wow", "tv", "desk", "couch", "table", "arrays!"]
   
 end
 
